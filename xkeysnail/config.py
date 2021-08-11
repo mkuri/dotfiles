676fd98839547a620885ab049800b5bd3c05bcf7
Makoto Kurihara <mkuri8m@gmail.com>
2021-08-12 04:41:05 +0900

    Remove config to remap capslock to ctrl at xkeysnail

--- xkeysnail/config.py
+++ xkeysnail/config.py
@@ -1,10 +1,6 @@
 import re
 from xkeysnail.transform import *
 
-define_modmap({
-    Key.CAPSLOCK: Key.LEFT_CTRL
-})
-
 define_multipurpose_modmap({
     # SandS
     Key.SPACE: [Key.SPACE, Key.LEFT_SHIFT],
