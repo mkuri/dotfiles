984fd328a675a6d3465b9883d78ff45e9ce785cb
Makoto Kurihara <mkuri8m@gmail.com>
2021-08-14 00:30:03 +0900

    Change C-h to global xkeysnail keymap

--- xkeysnail/config.py
+++ xkeysnail/config.py
@@ -10,10 +10,13 @@ define_multipurpose_modmap({
     Key.RIGHT_ALT: [Key.HENKAN, Key.RIGHT_ALT]
 })
 
+define_keymap(None, {
+    K("C-h"): K("Backspace"),
+}, "Global")
+
 define_keymap(re.compile("Google-chrome|Brave-browser"), {
     K("C-n"): K("Down"),
     K("C-p"): K("Up"),
     K("C-m"): K("Enter"),
-    K("C-h"): K("Backspace"),
     K("C-Shift-p"): K("C-p"),
 }, "Browser")
