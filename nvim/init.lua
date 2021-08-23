595ae99cdb7768a9ce6b8f2f7d4fc53555447ede
Makoto Kurihara <mkuri8m@gmail.com>
2021-08-23 15:07:48 +0900

    Add neovim config to yank to system clipboard

--- nvim/init.lua
+++ nvim/init.lua
@@ -18,6 +18,7 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+vim.opt.clipboard = "unnamedplus"
 
 ---------- color -------------------- 
 vim.cmd 'colorscheme iceberg'
