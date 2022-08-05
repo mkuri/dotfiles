8f731ffd5e5c312e5f173294e259a2d31b458776
Makoto Kurihara <mkuri8m@gmail.com>
2022-08-06 06:38:23 +0900

    feat(nvim): add jump to github blame page
    Signed-off-by: Makoto Kurihara <mkuri8m@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,6 +23,7 @@ require('packer').startup(function()
   }
   use 'numToStr/Comment.nvim'
   use 'ntpeters/vim-better-whitespace'
+  use 'hoshinotsuyoshi/vim-to-github'
   use 'cocopon/iceberg.vim'
 end)
 
@@ -40,8 +41,8 @@ vim.cmd 'colorscheme iceberg'
 ---------- keymaps --------------------
 vim.keymap.set('n', '<space>f', '<cmd>FzfLua files<CR>')
 vim.keymap.set('n', '<space>b', '<cmd>FzfLua buffers<CR>')
-vim.keymap.set('n', '<space>g', '<cmd>FzfLua live_grep<CR>')
 vim.keymap.set('n', '<space><space>', '<cmd>FzfLua builtin<CR>')
+vim.keymap.set('n', '<space>sg', '<cmd>FzfLua live_grep<CR>')
 vim.keymap.set('n', '<space>ss', '<cmd>FzfLua lsp_document_symbols<CR>')
 vim.keymap.set('n', '<space>sd', '<cmd>FzfLua diagnostics_document<CR>')
 vim.keymap.set('n', '<space>sc', '<cmd>FzfLua git_bcommits<CR>')
@@ -57,6 +58,7 @@ vim.keymap.set('n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<CR>')
 vim.keymap.set('n', '<space>tt', '<cmd>NvimTreeToggle<CR>')
 vim.keymap.set('n', '<space>to', '<cmd>NvimTreeFocus<CR>')
 vim.keymap.set('n', '<space>tf', '<cmd>NvimTreeFindFile<CR>')
+vim.keymap.set('n', '<space>gb', '<cmd>ToGithub<CR>')
 vim.keymap.set('n', '<esc><esc>', '<cmd>nohlsearch<CR>')
 
 ---------- lsp --------------------
