6d7392c251fafa3b32f02d70d8e8e5a5436e9936
Makoto Kurihara <mkuri8m@gmail.com>
2024-04-21 17:15:47 +0900

    feat(setup-dotfiles): add settings for i3-like workspace experience
    Signed-off-by: Makoto Kurihara <mkuri8m@gmail.com>

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -82,3 +82,23 @@ fi
 
 # gnome settings
 gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
+
+# For i3-like workspace experience
+dconf write /org/gnome/mutter/dynamic-workspaces false
+dconf write /org/gnome/desktop/wm/preferences/num-workspaces 10
+
+for i in {1..10}
+do
+   dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-$i "['<Super>$i']"
+   # Remove default binding
+   dconf write /org/gnome/shell/keybindings/switch-to-application-$i "@as []"
+done
+# Workspace 10 -> 0
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-10 "['<Super>0']"
+
+for i in {1..10}
+do
+   dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-$i "['<Super><Shift>$i']"
+done
+# Workspace 10 -> 0
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-10 "['<Super><Shift>0']"
